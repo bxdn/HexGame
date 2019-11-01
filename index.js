3b50d83c2ea5fb078f33f48f4a903c4c56eda1a6
bxdn <seanrsmo@yahoo.com>
2019-11-01 08:46:31 -0400

    Zoom and listeners

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ function scroll(event) {
     const scaleStr = map.style.transform ? map.style.transform : "scale(1)";
     const scale = Number(scaleStr.slice(6, scaleStr.indexOf(")")));
     if (delta > 0) {
-        map.style.transform = `scale(${scale - .1})`;
+        map.style.transformOrigin = "center";
+        map.style.transform = `scale(${scale * .9})`;
     }
     else if (delta < 0) {
-        map.style.transform = `scale(${scale + .1})`;
+        map.style.transformOrigin = "center";
+        map.style.transform = `scale(${scale * 1.1})`;
     }
 }
 function keyDown(event) {
@@ -23,36 +25,47 @@ function keyDown(event) {
     const map = document.getElementById("map");
     switch (event.code) {
         case "KeyS":
+            if (event.repeat) {
+                clearInterval(moveDown);
+            }
             moveDown = setInterval(function () {
-                const marginTopStr = map.style.marginTop ? map.style.marginTop : "0px";
-                const marginTop = Number(marginTopStr.slice(0, marginTopStr.indexOf("p")));
-                map.style.marginTop = `${marginTop + 1}px`;
+                const topStr = map.style.top ? map.style.top : "0px";
+                const top = Number(topStr.slice(0, topStr.indexOf("p")));
+                map.style.top = `${top - 1}px`;
             }, 5);
             break;
         case "KeyW":
+            if (event.repeat) {
+                clearInterval(moveUp);
+            }
             moveUp = setInterval(function () {
-                const marginTopStr = map.style.marginTop ? map.style.marginTop : "0px";
-                const marginTop = Number(marginTopStr.slice(0, marginTopStr.indexOf("p")));
-                map.style.marginTop = `${marginTop - 1}px`;
+                const topStr = map.style.top ? map.style.top : "0px";
+                const top = Number(topStr.slice(0, topStr.indexOf("p")));
+                map.style.top = `${top + 1}px`;
             }, 5);
             break;
         case "KeyA":
+            if (event.repeat) {
+                clearInterval(moveLeft);
+            }
             moveLeft = setInterval(function () {
-                const marginLeftStr = map.style.marginLeft ? map.style.marginLeft : "0px";
-                const marginLeft = Number(marginLeftStr.slice(0, marginLeftStr.indexOf("p")));
-                map.style.marginLeft = `${marginLeft - 1}px`;
+                const leftStr = map.style.left ? map.style.left : "0px";
+                const left = Number(leftStr.slice(0, leftStr.indexOf("p")));
+                map.style.left = `${left + 1}px`;
             }, 5);
             break;
         case "KeyD":
+            if (event.repeat) {
+                clearInterval(moveRight);
+            }
             moveRight = setInterval(function () {
-                const marginLeftStr = map.style.marginLeft ? map.style.marginLeft : "0px";
-                const marginLeft = Number(marginLeftStr.slice(0, marginLeftStr.indexOf("p")));
-                map.style.marginLeft = `${marginLeft + 1}px`;
+                const leftStr = map.style.left ? map.style.left : "0px";
+                const left = Number(leftStr.slice(0, leftStr.indexOf("p")));
+                map.style.left = `${left - 1}px`;
             }, 5);
     }
 }
 function keyUp(event) {
-    console.log("HAAA");
     switch (event.code) {
         case "KeyS":
             clearInterval(moveDown);
@@ -67,9 +80,47 @@ function keyUp(event) {
             clearInterval(moveRight);
     }
 }
-var noise = new SimplexNoise("hello");
-console.log(noise.noise2D(1, 2));
+function sumOcatave(noiseGen, num_iterations, x, y, persistence, scale) {
+    var maxAmp = 0.0;
+    var amp = 1.0;
+    var freq = scale;
+    var noise = 0.0;
+    for (let i = 0; i < num_iterations; i++) {
+        noise += noiseGen.noise2D(x * freq, y * freq) * amp;
+        maxAmp += amp;
+        amp *= persistence;
+        freq *= 2;
+    }
+    noise /= maxAmp;
+    return noise;
+}
+const noise = new SimplexNoise();
 const mapContainer = document.getElementById("mapContainer");
 mapContainer.addEventListener("wheel", scroll);
 window.addEventListener("keydown", keyDown);
 window.addEventListener("keyup", keyUp);
+const map = [];
+for (let i = 0; i < 33; i++) {
+    const row = [];
+    for (let j = 0; j < 99; j++) {
+        row.push(sumOcatave(noise, 16, i, j / 3, .5, .05));
+    }
+    map.push(row);
+}
+console.log(map);
+const green = new Image(100, 100);
+green.src = "GreenHex.png";
+const blue = new Image(100, 100);
+blue.src = "BlueHex.png";
+for (let i = 0; i < 99; i++) {
+    for (let j = 0; j < 33; j++) {
+        const x = (i % 2 == 0) ? j * 150 : j * 150 + 75;
+        const y = 50 * i;
+        const img = new Image(100, 100);
+        img.src = (map[j][i] > .1) ? "GreenHex.png" : "BlueHex.png";
+        img.style.left = `${x}px`;
+        img.style.top = `${y}px`;
+        img.style.position = "absolute";
+        document.getElementById("map").appendChild(img);
+    }
+}
--- index.ts
+++ index.ts
@@ -9,51 +9,64 @@ function scroll(event: WheelEvent) {
     const scaleStr = map.style.transform ? map.style.transform : "scale(1)"
     const scale = Number(scaleStr.slice(6, scaleStr.indexOf(")")))
     if (delta > 0) {
-        map.style.transform = `scale(${scale - .1})`
+        map.style.transformOrigin = "center"
+        map.style.transform = `scale(${scale * .9})`
     } else if (delta < 0) {
-        map.style.transform = `scale(${scale + .1})`
+        map.style.transformOrigin = "center"
+        map.style.transform = `scale(${scale * 1.1})`
     }
 }
 
 function keyDown(event: KeyboardEvent) {
-    if(event.repeat){
+    if (event.repeat) {
         return
     }
     console.log(event.code)
     const map = document.getElementById("map")
     switch (event.code) {
         case "KeyS":
+                if (event.repeat) {
+                    clearInterval(moveDown)
+                }
             moveDown = setInterval(function () {
-                const marginTopStr = map.style.marginTop ? map.style.marginTop : "0px"
-                const marginTop = Number(marginTopStr.slice(0, marginTopStr.indexOf("p")))
-                map.style.marginTop = `${marginTop + 1}px`
+                const topStr = map.style.top ? map.style.top : "0px"
+                const top = Number(topStr.slice(0, topStr.indexOf("p")))
+                map.style.top = `${top - 1}px`
             }, 5)
             break;
         case "KeyW":
+                if (event.repeat) {
+                    clearInterval(moveUp)
+                }
             moveUp = setInterval(function () {
-                const marginTopStr = map.style.marginTop ? map.style.marginTop : "0px"
-                const marginTop = Number(marginTopStr.slice(0, marginTopStr.indexOf("p")))
-                map.style.marginTop = `${marginTop - 1}px`
+                const topStr = map.style.top ? map.style.top : "0px"
+                const top = Number(topStr.slice(0, topStr.indexOf("p")))
+                map.style.top = `${top + 1}px`
             }, 5)
             break;
         case "KeyA":
+                if (event.repeat) {
+                    clearInterval(moveLeft)
+                }
             moveLeft = setInterval(function () {
-                const marginLeftStr = map.style.marginLeft ? map.style.marginLeft : "0px"
-                const marginLeft = Number(marginLeftStr.slice(0, marginLeftStr.indexOf("p")))
-                map.style.marginLeft = `${marginLeft - 1}px`
+                const leftStr = map.style.left ? map.style.left : "0px"
+                const left = Number(leftStr.slice(0, leftStr.indexOf("p")))
+                map.style.left = `${left + 1}px`
             }, 5)
             break;
         case "KeyD":
+                if (event.repeat) {
+                    clearInterval(moveRight)
+                }
             moveRight = setInterval(function () {
-                const marginLeftStr = map.style.marginLeft ? map.style.marginLeft : "0px"
-                const marginLeft = Number(marginLeftStr.slice(0, marginLeftStr.indexOf("p")))
-                map.style.marginLeft = `${marginLeft + 1}px`
+                const leftStr = map.style.left ? map.style.left : "0px"
+                const left = Number(leftStr.slice(0, leftStr.indexOf("p")))
+                map.style.left = `${left - 1}px`
             }, 5)
     }
 }
 
 function keyUp(event: KeyboardEvent) {
-    console.log("HAAA")
     switch (event.code) {
         case "KeyS":
             clearInterval(moveDown)
@@ -69,10 +82,51 @@ function keyUp(event: KeyboardEvent) {
     }
 }
 
+function sumOcatave(noiseGen: SimplexNoise, num_iterations: number, x: number, y: number, persistence: number, scale: number): number {
+    var maxAmp = 0.0
+    var amp = 1.0
+    var freq = scale
+    var noise = 0.0
+    for (let i = 0; i < num_iterations; i++) {
+        noise += noiseGen.noise2D(x * freq, y * freq) * amp
+        maxAmp += amp
+        amp *= persistence
+        freq *= 2
+    }
+    noise /= maxAmp
+    return noise
+}
 
-var noise = new SimplexNoise("hello");
-console.log(noise.noise2D(1, 2))
+const noise = new SimplexNoise()
 const mapContainer = document.getElementById("mapContainer")
-mapContainer.addEventListener("wheel", scroll);
-window.addEventListener("keydown", keyDown);
-window.addEventListener("keyup", keyUp);
+mapContainer.addEventListener("wheel", scroll)
+window.addEventListener("keydown", keyDown)
+window.addEventListener("keyup", keyUp)
+
+const map = []
+for (let i = 0; i < 33; i++) {
+    const row = []
+    for (let j = 0; j < 99; j++) {
+        row.push(sumOcatave(noise, 16, i, j / 3, .5, .05))
+    }
+    map.push(row)
+}
+console.log(map)
+
+const green = new Image(100, 100)
+green.src = "GreenHex.png"
+const blue = new Image(100, 100)
+blue.src = "BlueHex.png"
+
+for (let i = 0; i < 99; i++) {
+    for (let j = 0; j < 33; j++) {
+        const x =  (i % 2 == 0) ? j * 150 : j * 150 + 75
+        const y = 50 * i
+        const img = new Image(100,100)
+        img.src = (map[j][i] > .1) ? "GreenHex.png" : "BlueHex.png"
+        img.style.left = `${x}px`
+        img.style.top = `${y}px`
+        img.style.position = "absolute"
+        document.getElementById("map").appendChild(img)
+    }
+}
